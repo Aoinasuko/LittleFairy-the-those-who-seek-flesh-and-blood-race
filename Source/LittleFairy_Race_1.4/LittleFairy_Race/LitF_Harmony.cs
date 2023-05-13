e907840e5e8a4627305cbb838c1a7458e7520bfa
Aoi_Nasuko <aoinasuko.nasunasu@gmail.com>
2023-05-14 02:50:37 +0900

    Core実装アプデ

--- Source/LittleFairy_Race_1.4/LittleFairy_Race/LitF_Harmony.cs
+++ Source/LittleFairy_Race_1.4/LittleFairy_Race/LitF_Harmony.cs
@@ -173,150 +173,6 @@ namespace LittleFairy_Race
         }
     }
 
-    // 肉を人間扱いにしなくなる
-    [HarmonyPatch(typeof(FoodUtility), "IsHumanlikeCorpseOrHumanlikeMeat")]
-    static class LitF_FixFoodUtility
-    {
-        [HarmonyPrefix]
-        public static bool Fix_IsHumanlike(ref bool __result, ref Thing source, ref ThingDef foodDef)
-        {
-            Corpse corpse = source as Corpse;
-            if (corpse != null && (corpse.InnerPawn.def.race.FleshType == FleshType_LitF.LitF_Fairy))
-            {
-                __result = false;
-                return false;
-            }
-            return true;
-        }
-    }
-
-    [HarmonyPatch(typeof(FoodUtility), "GetMeatSourceCategoryFromCorpse")]
-    [HarmonyPatch(new Type[]
-    {
-        typeof(Thing)
-    })]
-    static class LitF_FixFoodUtility_GetMeatSourceCategoryCorpse
-    {
-        [HarmonyPrefix]
-        public static bool Fix_IsHumanlike(ref MeatSourceCategory __result, Thing thing)
-        {
-            Corpse corpse = thing as Corpse;
-            if (corpse != null && corpse.InnerPawn.def.race.FleshType == FleshType_LitF.LitF_Fairy)
-            {
-                __result = MeatSourceCategory.Undefined;
-                return false;
-            }
-            return true;
-        }
-    }
-
-    [HarmonyPatch(typeof(FoodUtility), "GetMeatSourceCategory")]
-    [HarmonyPatch(new Type[]
-    {
-        typeof(ThingDef)
-    })]
-    static class LitF_FixFoodUtility_GetMeatSourceCategory
-    {
-        [HarmonyPrefix]
-        public static bool Fix_IsHumanlike(ref MeatSourceCategory __result, ThingDef source)
-        {
-            if (source.ingestible == null)
-            {
-                return true;
-            }
-            if ((source.ingestible.foodType & FoodTypeFlags.Meat) == FoodTypeFlags.Meat)
-            {
-                if (source.ingestible.sourceDef != null)
-                {
-                    if (source.ingestible.sourceDef.race.FleshType == FleshType_LitF.LitF_Fairy)
-                    {
-                        __result = MeatSourceCategory.Undefined;
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
-    }
-
-    [HarmonyPatch(typeof(Corpse), "ButcherProducts")]
-    [HarmonyPatch(new Type[] { typeof(Pawn), typeof(float) })]
-    static class LitF_Corpse_Patch
-    {
-
-        [HarmonyBefore(new string[] { "rimworld.erdelf.alien_race.main" })]
-        [HarmonyPrefix]
-        static bool Prefix(Pawn butcher, float efficiency, ref IEnumerable<Thing> __result, Corpse __instance)
-        {
-
-            if (__instance.InnerPawn.def.race.FleshType == FleshType_LitF.LitF_Fairy)
-            {
-                Pawn corpse = __instance.InnerPawn;
-                IEnumerable<Thing> enumerable = corpse.ButcherProducts(butcher, efficiency);
-                if (corpse.RaceProps.BloodDef != null)
-                {
-                    FilthMaker.TryMakeFilth(butcher.Position, butcher.Map, corpse.RaceProps.BloodDef, corpse.LabelIndefinite());
-                }
-                __result = enumerable;
-                return false;
-            }
-            return true;
-        }
-
-    }
-
-    // 死体に耐性をつける
-    [HarmonyPatch(typeof(CorpseObsessionMentalStateUtility), "IsCorpseValid")]
-    [HarmonyPatch(new Type[]
-    {
-        typeof(Corpse),
-        typeof(Pawn),
-        typeof(bool),
-    })]
-    internal static class CorpseObsession_Patch
-    {
-
-        [HarmonyPrefix]
-        static bool Prefix(ref bool __result, Corpse corpse, Pawn pawn, bool ignoreReachability)
-        {
-            if (corpse == null)
-            {
-                return true;
-            }
-            if (corpse.InnerPawn.def.defName == "LittleFairy_Pawn")
-            {
-                __result = false;
-                return false;
-            }
-            return true;
-        }
-    }
-
-    [HarmonyPatch(typeof(Corpse), "GiveObservedHistoryEvent")]
-    [HarmonyPatch(new Type[]
-    {
-            typeof(Pawn),
-    })]
-    internal static class Corpse_Patch
-    {
-
-        [HarmonyPrefix]
-        static bool Prefix(ref Corpse __instance, ref HistoryEventDef __result, Pawn observer)
-        {
-            if (__instance.InnerPawn.def.defName == "LittleFairy_Pawn")
-            {
-                if (!observer.story.traits.HasTrait(TraitDefOf.Kind))
-                {
-                    __result = null;
-                    return false;
-                }
-            }
-            return true;
-        }
-    }
-
-
-
     // リトルフェアリーの場合、バーサーカー状態なら近接クールダウンを倍進める
     [HarmonyPatch(typeof(Stance_Cooldown), "StanceDraw")]
     static class Patch_Test
@@ -398,30 +254,6 @@ namespace LittleFairy_Race
         }
     }
 
-    // 死は仲がいいやつ以外誰も悲しまない
-    [HarmonyPatch(typeof(PawnDiedOrDownedThoughtsUtility), "AppendThoughts_ForHumanlike")]
-    [HarmonyPatch(new Type[]
-    {
-        typeof(Pawn),
-        typeof(DamageInfo),
-        typeof(PawnDiedOrDownedThoughtsKind),
-        typeof(List<IndividualThoughtToAdd>),
-        typeof(List<ThoughtToAddToAll>),
-    })]
-    public static class LitF_PreDead
-    {
-        [HarmonyPrefix]
-        public static bool LitF_PreApplyDead(Pawn victim, DamageInfo? dinfo, PawnDiedOrDownedThoughtsKind thoughtsKind, List<IndividualThoughtToAdd> outIndividualThoughts, List<ThoughtToAddToAll> outAllColonistsThoughts)
-        {
-            if (victim.def.race.FleshType == FleshType_LitF.LitF_Fairy)
-            {
-                return false;
-            }
-            return true;
-        }
-
-    }
-
     /// <summary>
     /// 脱走しない
     /// </summary>
@@ -437,11 +269,6 @@ namespace LittleFairy_Race
         [HarmonyPrefix]
         static bool Prefix(ref float __result, Pawn pawn, StringBuilder sb, bool ignoreAsleep)
         {
-            if (pawn.def.defName == "LittleFairy_Pawn")
-            {
-                __result = -1f;
-                return false;
-            }
             if (ModsConfig.IdeologyActive)
             {
                 if (pawn.ideo?.Ideo?.memes.Contains(Ideology_LitF.LitF_Fairy) ?? false)
@@ -464,11 +291,6 @@ namespace LittleFairy_Race
         [HarmonyPrefix]
         static bool Prefix(ref float __result, Pawn pawn)
         {
-            if (pawn.def.defName == "LittleFairy_Pawn")
-            {
-                __result = -1f;
-                return false;
-            }
             if (ModsConfig.IdeologyActive)
             {
                 if (pawn.ideo?.Ideo?.memes.Contains(Ideology_LitF.LitF_Fairy) ?? false)
@@ -491,156 +313,12 @@ namespace LittleFairy_Race
         [HarmonyPrefix]
         static bool Prefix(ref float __result, Pawn pawn)
         {
-            if (pawn.def.defName == "LittleFairy_Pawn")
-            {
-                __result = -1f;
-                return false;
-            }
             if (ModsConfig.IdeologyActive)
             {
                 if (pawn.ideo?.Ideo?.memes.Contains(Ideology_LitF.LitF_Fairy) ?? false)
                 {
                     __result = -1f;
                     return false;
-                }
-            }
-            return true;
-        }
-    }
-
-    // リトルフェアリーの派閥追加時、服装規定を無効化する
-    [HarmonyPatch(typeof(Precept_Role), "GenerateNewApparelRequirements")]
-    [HarmonyPatch(new Type[]
-    {
-        typeof(FactionDef),
-    })]
-    internal static class ApparelRequirement_Patch
-    {
-
-        [HarmonyPrefix]
-        static bool Prefix(ref Precept_Role __instance, ref List<PreceptApparelRequirement> __result, FactionDef generatingFor)
-        {
-            if (generatingFor != null)
-            {
-                if (generatingFor.defName == "LitF_BloodyWildLittleFairy")
-                {
-                    List<PreceptApparelRequirement> ApparelRequirement = new List<PreceptApparelRequirement>();
-                    // 役割のdefName取得
-                    String role = __instance.def.defName;
-                    // 役割によって装備を指定
-                    if (role == "IdeoRole_Leader")
-                    {
-                        PreceptApparelRequirement item = new PreceptApparelRequirement();
-                        item.requirement = new ApparelRequirement();
-                        item.requirement.bodyPartGroupsMatchAny = new List<BodyPartGroupDef>();
-                        item.requirement.requiredDefs = new List<ThingDef>();
-                        item.requirement.bodyPartGroupsMatchAny.Add(BodyPartGroupDefOf.Torso);
-                        item.requirement.requiredDefs.Add(ThingDef.Named("LitF_BloodyArmor"));
-                        ApparelRequirement.Add(item);
-                    }
-                    if (!ApparelRequirement.NullOrEmpty())
-                    {
-                        __result = ApparelRequirement;
-                    }
-                    else
-                    {
-                        __result = null;
-                    }
-                    return false;
-                }
-                if (generatingFor.defName == "LitF_GentleWildLittleFairy")
-                {
-                    List<PreceptApparelRequirement> ApparelRequirement = new List<PreceptApparelRequirement>();
-                    // 役割のdefName取得
-                    String role = __instance.def.defName;
-                    // 役割によって装備を指定
-                    if (!ApparelRequirement.NullOrEmpty())
-                    {
-                        __result = ApparelRequirement;
-                    }
-                    else
-                    {
-                        __result = null;
-                    }
-                    return false;
-                }
-            }
-            return true;
-        }
-    }
-
-    /// <summary>
-    /// 奴隷時に作業速度ボーナス
-    /// <summary>
-    [StaticConstructorOnStartup]
-    [HarmonyPatch(typeof(StatPart_Slave))]
-    internal static class TransformValue_Patch
-    {
-        [HarmonyPrefix]
-        [HarmonyPatch(nameof(StatPart_Slave.TransformValue),
-            new Type[] { typeof(StatRequest), typeof(float) },
-            new ArgumentType[] { ArgumentType.Normal, ArgumentType.Ref })]
-        static bool Prefix(StatRequest req, ref float val)
-        {
-            if (req.HasThing)
-            {
-                if (req.Thing as Pawn != null)
-                {
-                    Pawn pawn = (Pawn)req.Thing;
-                    if (pawn.def.defName == "LittleFairy_Pawn")
-                    {
-                        Need_Suppression need_Suppression = pawn.needs.TryGetNeed<Need_Suppression>();
-                        if (need_Suppression != null)
-                        {
-                            int multi = (int)Math.Min(5, need_Suppression.CurLevel * 100 / 10);
-                            val *= (float)1.0f + multi * 0.1f;
-                            return false;
-                        }
-                    }
-                    if (pawn.ideo?.Ideo?.memes.Contains(Ideology_LitF.LitF_Fairy) ?? false)
-                    {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
-    }
-
-    /// <summary>
-    /// 奴隷時のボーナス説明
-    /// <summary>
-    [HarmonyPatch(typeof(StatPart_Slave), "ExplanationPart")]
-    [HarmonyPatch(new Type[]
-    {
-        typeof(StatRequest),
-    })]
-    internal static class ExplanationPart_Patch
-    {
-        [HarmonyPrefix]
-        static bool Prefix(ref StatPart_Slave __instance, ref String __result, StatRequest req)
-        {
-            if (req.HasThing)
-            {
-                if (req.Thing as Pawn != null)
-                {
-
-                    Pawn pawn = (Pawn)req.Thing;
-                    if (pawn.def.defName == "LittleFairy_Pawn")
-                    {
-                        Need_Suppression need_Suppression = pawn.needs.TryGetNeed<Need_Suppression>();
-                        if (need_Suppression != null)
-                        {
-                            int multi = (int)Math.Min(5, need_Suppression.CurLevel * 100 / 10);
-                            float parc = (float)1.0f + multi * 0.1f;
-                            __result = "LitF.UI.Excitement".Translate() + (": x" + parc.ToStringPercent());
-                            return false;
-                        }
-                    }
-                    if (pawn.ideo?.Ideo?.memes.Contains(Ideology_LitF.LitF_Fairy) ?? false)
-                    {
-                        return false;
-                    }
                 }
             }
             return true;
@@ -673,46 +351,4 @@ namespace LittleFairy_Race
             return true;
         }
     }
-
-    /// <summary>
-    /// 年齢補正無効化
-    /// <summary>
-    [HarmonyPatch(typeof(StatPart_Age), "AgeMultiplier")]
-    [HarmonyPatch(new Type[]
-    {
-        typeof(Pawn),
-    })]
-    internal static class AgeMultiplier_Patch
-    {
-        [HarmonyPrefix]
-        static bool Prefix(ref StatPart_Age __instance, ref float __result, Pawn pawn)
-        {
-            if (pawn.def.defName == "LittleFairy_Pawn")
-            {
-                __result = 1.0f;
-                return false;
-            }
-            return true;
-        }
-    }
-
-    [HarmonyPatch(typeof(StatPart_AgeOffset), "AgeOffset")]
-    [HarmonyPatch(new Type[]
-    {
-        typeof(Pawn),
-    })]
-    internal static class AgeMultiplier_Patch_Offset
-    {
-        [HarmonyPrefix]
-        static bool Prefix(ref StatPart_AgeOffset __instance, ref float __result, Pawn pawn)
-        {
-            if (pawn.def.defName == "LittleFairy_Pawn")
-            {
-                __result = 0f;
-                return false;
-            }
-            return true;
-        }
-    }
-
 }
